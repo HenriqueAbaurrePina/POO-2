7d68a9a33971b94cd14c7ed9f0368642c0ad71a4
Henrique Pina <103440192+HenriqueAbaurrePina@users.noreply.github.com>
2023-03-07 19:52:19 -0300

    Update Conta.cs

--- Conta.cs
+++ Conta.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 class Conta{
     private string nome;
     private string cpf;
     private double saldo;
-    //private double extrato;
-    
+    List<(string, double)> transacoes = new List<(string, double)>();
+
     public Conta(){
         Nome = "";
         Cpf = "";
@@ -30,7 +31,22 @@ class Conta{
     }
 
     public double Sacar (double valor) {
+        transacoes.Add(("Saque", valor));
         return Saldo = Saldo - valor;
         
+    }
+    public double Depositar (double valor) {
+        transacoes.Add(("Deposito", valor));
+        return Saldo = Saldo + valor;
+        
+    }
+    
+    public void Extrato()
+    {
+        Console.WriteLine("Transações realizadas:");
+        foreach (var transacao in transacoes) {
+            Console.WriteLine($"{transacao.Item1}: R${transacao.Item2}");
+        }
+        Console.WriteLine($"Saldo atual: R${saldo}");
     }
 }
